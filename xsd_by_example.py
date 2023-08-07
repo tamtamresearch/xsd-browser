1c71845005bda8830b5b50023b480f9daf87f230
David Koňařík <dvdkon@konarici.cz>
2023-08-07 10:36:49 +0200

    Add support for substitution groups on elements

--- xsd_by_example.py
+++ xsd_by_example.py
@@ -42,6 +42,7 @@ def elem_path_attrs(elem):
     path = elem_path(elem)
     return {
         "data-name": path[0],
+        "data-substgroups": elem.attrib.get("substitutionGroup", ""),
         "data-path": "/".join(path),
     }
 
